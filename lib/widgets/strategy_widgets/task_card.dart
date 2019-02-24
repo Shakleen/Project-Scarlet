277d44773c2a03856656e9c60f6a66b26365855b
ishrak <shakleenishfar@gmail.com>
2019-02-24 23:00:34 +0600

    24 February, 2019 - Minor design change to task card.

--- lib/widgets/strategy_widgets/task_card.dart
+++ lib/widgets/strategy_widgets/task_card.dart
@@ -5,27 +5,59 @@ import '../../pages/strategy_pages/task_form_page.dart';
 import '../../entities/task_entity.dart';
 import '../../scoped_model/task_model.dart';
 
-class TaskCard extends StatelessWidget {
+class TaskCard extends StatefulWidget {
   final TaskEntity task;
   final int tabNumber;
 
   TaskCard(this.task, this.tabNumber);
+
+  @override
+  _TaskCard createState() {
+    // TODO: implement createState
+    return _TaskCard();
+  }
+}
+
+class _TaskCard extends State<TaskCard> {
+  IconData infoIcon;
+  bool moreInfo;
+  Color infoIconColor;
+  Color backgroundColor;
+
+  @override
+  void initState() {
+    moreInfo = false;
+    infoIcon = Icons.expand_more;
+    infoIconColor = Colors.black;
+    backgroundColor = Colors.white;
+    super.initState();
+  }
+
+  void _setState() {
+    setState(() {
+      moreInfo = !moreInfo;
+      infoIcon = moreInfo ? Icons.expand_less : Icons.expand_more;
+      infoIconColor = moreInfo ? Colors.blue : Colors.black;
+      backgroundColor =moreInfo ? Colors.lightBlue[50] : Colors.white;
+    });
+  }
 
   /// Method for building the entire widget.
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: _buildDecorations(),
-      child: _buildTaskCard(context, task),
+      child: _buildTaskCard(context),
       padding: EdgeInsets.symmetric(vertical: 5.0),
     );
   }
 
-  Column _buildTaskCard(BuildContext context, TaskEntity task) {
-    final double deviceWidth = MediaQuery.of(context).size.width * 0.8;
+  Column _buildTaskCard(BuildContext context) {
+    final double deviceWidth = MediaQuery.of(context).size.width * 0.6;
     final IconData priorityIcon =
-        TaskModel.priorityLevels[task.getPriority()][1];
-    final Color priorityColor = TaskModel.priorityLevels[task.getPriority()][2];
+        TaskModel.priorityLevels[widget.task.getPriority()][1];
+    final Color priorityColor =
+        TaskModel.priorityLevels[widget.task.getPriority()][2];
 
     return Column(
       children: <Widget>[
@@ -33,7 +65,7 @@ class TaskCard extends StatelessWidget {
         ListTile(
           title: _buildInformationText(
             priorityIcon,
-            task.getName(),
+            widget.task.getName(),
             deviceWidth,
             26.0,
             25.0,
@@ -41,13 +73,20 @@ class TaskCard extends StatelessWidget {
             Colors.black,
           ),
           subtitle: _buildTrailer(deviceWidth),
+          trailing: IconButton(
+            icon: Icon(infoIcon, size: 25,),
+            color: infoIconColor,
+            onPressed: () {
+              _setState();
+            },
+          ),
           enabled: true,
           onLongPress: () {
-            if (tabNumber != 2) {
+            if (widget.tabNumber != 2) {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => TaskForm(task),
+                  builder: (context) => TaskForm(widget.task),
                 ),
               );
             }
@@ -58,14 +97,14 @@ class TaskCard extends StatelessWidget {
   }
 
   Widget _buildTrailer(double deviceWidth) {
-    final String description = task.getDescription();
-    final String location = task.getLocation();
+    final String description = widget.task.getDescription();
+    final String location = widget.task.getLocation();
     final List<Widget> childrenList = [];
 
     // Adding duedate
     childrenList.add(_buildInformationText(
       Icons.access_time,
-      _formatDateTime(task.getDueDate()),
+      _formatDateTime(widget.task.getDueDate()),
       deviceWidth,
       14.0,
       20.0,
@@ -73,7 +112,8 @@ class TaskCard extends StatelessWidget {
       Colors.black,
     ));
 
-    if (description != null && description != 'null') {
+    if (moreInfo) {
+      if (description != null && description != 'null') {
       childrenList.add(_buildInformationText(
         Icons.format_align_left,
         description,
@@ -96,6 +136,8 @@ class TaskCard extends StatelessWidget {
         Colors.black,
       ));
     }
+    }
+
     return Column(
       children: childrenList,
     );
@@ -146,7 +188,7 @@ class TaskCard extends StatelessWidget {
           style: BorderStyle.solid,
         ),
       ),
-      color: Colors.white,
+      color: backgroundColor,
     );
   }
 
